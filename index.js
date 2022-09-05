5cc71c5cbc0db1c7869ba8f8941b1d67d4bf7e87
Ihsanullah Mahboobi <iumahboobi@gmail.com>
2022-09-05 13:30:06 +0200

    update

--- index.js
+++
@@ -1,7 +0,0 @@
-import './styles/main.scss'; // Don't delete :)
-import smile from './scripts/smile.js'; // Feel free to delete :)
-
-// All of your javascript should go here :)
-
-
-smile() // Feel free to delete :)
